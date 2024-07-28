e1b02b99343a39c3800e0f8246dd4abb2edb1d1f
Nanor-0196-Bright-415-Nanor <147738677+Nanor-0196-Bright-415-Nanor@users.noreply.github.com>
2024-07-28 20:52:39 +0000

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import HeroImg from './Components/HeroImg'
+import LoginPage from './Components/LoginPage'
+
+
+function App() {
+  return (
+    <div className='main__container'>
+        <HeroImg />
+        <LoginPage />  
+    </div>
+  )
+}
+
+export default App
