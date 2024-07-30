bb5053cb61dbe21aaf5d43fad31a5d1d7e65fcb6
Nanor-0196-Bright-415-Nanor <147738677+Nanor-0196-Bright-415-Nanor@users.noreply.github.com>
2024-07-30 04:29:42 +0000

    Required changes (on Layout structure)

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,56 @@
-import React from 'react'
-import HeroImg from './Components/HeroImg'
-import LoginPage from './Components/LoginPage'
+ import React from "react"
 
+ export default function App(){
 
-function App() {
   return (
-    <div className='main__container'>
-        <HeroImg />
-        <LoginPage />  
+    <div className="App">
+      <div className="App__img">
+      <div className="logo__wrapper">
+      <img src="/Images/Logo.png" alt="Welcome Logo" className="logo"/>
+      <h1> WelcomInApp</h1>
+      </div>
+          <img src="/Images/HeroImage.png" alt="Picture"  />
+          <h2>Easily manage your <br />property today</h2>
+      </div>
+      <div className="App__logPage">
+        <div className="App__logWrapper"> 
+          <h3 className="App__heading">Log in</h3>
+          <form>
+          <label htmlFor="useremail">Email</label>
+          <input 
+          type="email" 
+          name="useremail"
+          id="useremail"
+          placeholder="Enter your email"
+          className="email"
+          />
+          <div className="password__wrapper"> 
+          <label htmlFor="userPassword"   className="password">Password
+            <span className="forget__password">Forgot password ?</span>
+          </label>
+          
+          <input 
+          type="password" 
+          name="userpassword"
+          id="userpassword"
+          placeholder="Enter your Password"
+        
+          />
+         <img src="/Images/EyeLogo.png" alt="Eye Logo" className="eye__logo"/>
+         </div>
+          <button className="btn">Log in</button>
+
+          <hr className="hr"/>
+
+          <div className="googleImg__wrapper">
+            <img src="/Images/Google Logo - Copy.jpeg" alt="Google Image Wrapper" />
+            <div>Continue with Gmail</div>
+          </div>
+           <p className="account__enquiry">Don't have an account ? <span className="signup"> Sign up </span>
+           </p>
+          </form>
+          </div>
+      </div>
     </div>
   )
-}
-
-export default App
+ }
